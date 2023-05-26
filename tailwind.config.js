84ebaebd33499bcd1d67f8a6f7eea729d6bbc2e2
Functions & Glory <functionsandglory@proton.me>
2023-05-26 07:06:18 -0700

    Minor styles changes. Adding twitter handle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,11 @@ module.exports = {
     content: [
         'index.html'
     ],
+    theme: {
+        extend: {
+            colors: {
+                'theme-red': '#b35959'
+            },
+        },
+    },
 };
