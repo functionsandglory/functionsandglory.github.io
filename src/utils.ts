b6e2249a614a5f2da309f5f3aa8590ccbbfe1499
Functions & Glory <functionsandglory@proton.me>
2024-04-10 19:32:31 -0700

    Adding contact and project section content.

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,17 @@
+import {SECTION_TITLE_HEADER_OFFSET} from './constants';
+
 export function getScrollContainer() {
     return document.getElementById('scroll-container');
 }
 
 export function easeIn(value: number) {
-   return Math.pow(value, 3)
+    return Math.pow(value, 3)
+}
+
+export function scrollToSection(scrollContainer: HTMLElement, targetElement: HTMLElement) {
+    const targetTop = Math.ceil(scrollContainer.scrollTop + (targetElement.getBoundingClientRect().y - SECTION_TITLE_HEADER_OFFSET));
+    scrollContainer.scrollTo({
+        top: targetTop,
+        behavior: 'smooth'
+    });
 }
